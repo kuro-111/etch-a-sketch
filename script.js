ead5ee0fc79ccaca623ad4c185a030916004c44b
kuro-111 <graceyann666@gmail.com>
2023-07-25 09:04:50 +0800

    Establish boilerplate and starting codes for sketch website

---
+++ script.js
@@ -0,0 +1,22 @@
+/*make div boxes to fit 16x16 exclusively in js
+    kill yourself*/
+
+
+
+
+
+
+//set variables
+const container = document.querySelector('.container');
+const row = document.getElementsByClassName('div');
+const column =  document.getElementsByClassName('div');
+//make box
+const boxes = document.createElement('div');
+//make boxes be container child
+
+container.appendChild(boxes);
+
+
+
+
+
