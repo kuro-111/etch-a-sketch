82f9eec84dc8506cb494c1f9eb2cdc58c7510def
kuro-111 <graceyann666@gmail.com>
2023-07-28 09:03:06 +0800

    Finish the functions for making the cells and add an event listener

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-/*make div boxes to fit 16x16 exclusively in js
-    kill yourself*/
+/*make div boxes to fit 16x16 exclusively in js */
 
 
 
@@ -8,13 +7,45 @@
 
 //set variables
 const container = document.querySelector('.container');
-const row = document.getElementsByClassName('div');
-const column =  document.getElementsByClassName('div');
+const rows = document.getElementsByClassName('div');
+const columns =  document.getElementsByClassName('div');
 //make box
-const boxes = document.createElement('div');
-//make boxes be container child
+function gridDefault() {
+    makeRows(16);
+    makeColumns(16);
+}
 
-container.appendChild(boxes);
+function makeRows(rowNum) {
+
+    for (i=0; i<rowNum; i++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = "div";
+    };
+}
+
+
+
+function makeColumns(cellNum) {
+    for (i=0; i<rows.length; i++) {
+        for (j=0; j<cellNum; j++) {
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = "cell";
+        };
+    };
+};
+
+gridDefault ();
+
+
+//make hover effect
+
+const box = document.querySelectorAll('.cell');
+box.forEach( box => box.addEventListener('mouseover',(event)=> {
+    event.target.classList.add('show');
+    
+
+}))
+
 
 
 
