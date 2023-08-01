9dff91aa5dc64c0fa080a642ffbe9cef9d541f58
kuro-111 <graceyann666@gmail.com>
2023-08-01 08:58:56 +0800

    Update event for dragging instead of hover

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
 /*make div boxes to fit 16x16 exclusively in js */
 
-
-
-
-
-
 //set variables
-const container = document.querySelector('.container');
-const rows = document.getElementsByClassName('div');
-const columns =  document.getElementsByClassName('div');
+const container = document.querySelector(".container");
+const rows = document.getElementsByClassName("div");
+const columns = document.getElementsByClassName("div");
+let currentlyClicking = false;
 //make box
 function gridDefault() {
-    makeRows(16);
-    makeColumns(16);
+     makeRows(16);
+     makeColumns(16);
 }
 
 function makeRows(rowNum) {
-
-    for (i=0; i<rowNum; i++) {
-        let row = document.createElement('div');
-        container.appendChild(row).className = "div";
-    };
+     for (i = 0; i < rowNum; i++) {
+          let row = document.createElement("div");
+          container.appendChild(row).className = "div";
+     }
 }
 
-
-
 function makeColumns(cellNum) {
-    for (i=0; i<rows.length; i++) {
-        for (j=0; j<cellNum; j++) {
-            let newCell = document.createElement('div');
-            rows[j].appendChild(newCell).className = "cell";
-        };
-    };
-};
+     for (i = 0; i < rows.length; i++) {
+          for (j = 0; j < cellNum; j++) {
+               let newCell = document.createElement("div");
+               rows[j].appendChild(newCell).className = "cell";
+          }
+     }
+}
 
-gridDefault ();
-
+gridDefault();
 
 //make hover effect
 
-const box = document.querySelectorAll('.cell');
-box.forEach( box => box.addEventListener('mouseover',(event)=> {
-    event.target.classList.add('show');
-    
+container.addEventListener("mousedown", (event) => {
+     currClicking = true;
+});
 
-}))
-
-
-
-
-
-
+const box = document.querySelectorAll(".cell");
+box.forEach((box) =>
+     container.addEventListener("mousemove", (event) => {
+          if (currClicking === true) {
+               event.target.classList.add("show");
+          }
+     })
+);
