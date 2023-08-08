d845e640c747602c65530f1c99f49d2bcc4eee24
kuro-111 <graceyann666@gmail.com>
2023-08-08 08:59:52 +0800

    Make resize feature functional and cell dynamic size

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const rows = document.getElementsByClassName("div");
 const columns = document.getElementsByClassName("div");
 let currClicking = false;
 //make box
+let defaultSize = true;
 function gridDefault() {
      makeRows(16);
      makeColumns(16);
@@ -22,7 +23,20 @@ function makeRows(rowNum) {
 function makeColumns(cellNum) {
      for (i = 0; i < rows.length; i++) {
           for (j = 0; j < cellNum; j++) {
+               // Create cell
                let newCell = document.createElement("div");
+
+               // Resize cell
+               let containerWidth = container.clientWidth;
+               let containerHeight = container.clientHeight;
+
+               let cellWidth = containerWidth / cellNum;
+               let cellHeight = containerHeight / rows.length;
+
+               // Use JS to style your cells width and height -
+               newCell.style.width = `${cellWidth}px`;
+               newCell.style.height = `${cellHeight}px`;
+               // Put cell in DOM
                rows[j].appendChild(newCell).className = "cell";
           }
      }
@@ -53,13 +67,17 @@ boxes.forEach((box) => {
      });
 });
 
+//resize grid
 const resize = document.querySelector(".resize");
 resize.addEventListener("click", (event) => {
-     let resizeInputRows = prompt("Rows: (under 200)");
-     let resizeInputColumn = prompt("Columns: (under 200)");
+     let resizeInputRows = prompt("Rows: (under 100)");
+     let resizeInputColumn = prompt("Columns: (under 100)");
 
      // Get rid of old grid
+     container.innerHTML = null;
 
      makeRows(resizeInputRows);
      makeColumns(resizeInputColumn);
+
+     boxes;
 });
